64a4f40273b272e25bb04adf54ed557558b06471
Muxriddin <smdplayer199@gmail.com>
2020-10-19 21:54:49 +0500

    third commit

---
+++ app/src/main/java/uz/smd/marsarchitected/network/MarsApiService.kt
@@ -0,0 +1,4 @@
+package uz.smd.marsarchitected.network
+
+private const val BASE_URL = "https://mars.udacity.com/"
+enum class MarsApiFilter(val value: String) { SHOW_RENT("rent"), SHOW_BUY("buy"), SHOW_ALL("all") }
