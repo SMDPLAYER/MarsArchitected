63ca3175d8c5eeb25e895dbefe574cce3a7137a5
Muxriddin <smdplayer199@gmail.com>
2020-10-19 21:39:53 +0500

    initial commit

---
+++ app/src/main/java/uz/smd/marsarchitected/app/App.kt
@@ -0,0 +1,13 @@
+package uz.smd.marsarchitected.app
+
+import android.app.Application
+
+class App:Application() {
+    companion object{
+        lateinit var instance: App
+    }
+    override fun onCreate() {
+        super.onCreate()
+        instance =this
+    }
+}
